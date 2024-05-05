559c1ec7d637c1c258322b306f928fa9e57219ea
ozzlamaz <a.osman4233@gmail.com>
2024-05-05 16:39:56 +0300

    Create Reusable Grid

---
+++ src/component/InfiniteItemGrid.tsx
@@ -0,0 +1,22 @@
+import { FetchResponse } from "../entities/FetchResponse";
+import { Movie } from "../entities/Movie";
+import { MovieCardComponent } from "./MovieCardComponent";
+
+interface Props {
+  pages?: FetchResponse<Movie>[];
+}
+
+const InfiniteItemGrid = ({ pages }: Props) => {
+  return (
+    <div className="row-gap-4 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5">
+      {pages?.map((page) =>
+        page.results.map((item) => (
+          <div key={item.id}>
+            <MovieCardComponent item={item}></MovieCardComponent>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+export default InfiniteItemGrid;
--- src/pages/DataGrid.tsx
+++ src/pages/DataGrid.tsx
@@ -2,14 +2,15 @@ import useMovies from "../hooks/useMovies";
 import { Filters } from "../component/Filters";
 import useMoviesQuery from "../state-management/MovieQueryStore";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { MovieCardComponent } from "../component/MovieCardComponent";
 import { SekeltonCard } from "../component/SekeltonCard";
+import InfiniteItemGrid from "../component/InfiniteItemGrid";
 
 export const DataGrid = () => {
   const movieQuery = useMoviesQuery((s) => s.movieQuery);
 
   const { data, fetchNextPage, hasNextPage, isLoading } = useMovies(movieQuery);
-  const pageLength = data?.pages.reduce((total, page) => total + page.results.length, 0) || 0;
+  const pageLength =
+    data?.pages.reduce((total, page) => total + page.results.length, 0) || 0;
 
   return (
     <div className="container my-4 px-6">
@@ -22,19 +23,8 @@ export const DataGrid = () => {
         hasMore={hasNextPage}
         loader={<div></div>}
       >
-        <div className="row-gap-4 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5">
-          {isLoading && <SekeltonCard count={10} />}
-
-          {data?.pages.map((page) => {
-            return page.results.map((movie) => {
-              return (
-                <div key={movie.id}>
-                  <MovieCardComponent item={movie}></MovieCardComponent>
-                </div>
-              );
-            });
-          })}
-        </div>
+        {isLoading && <SekeltonCard count={10} />}
+        <InfiniteItemGrid pages={data?.pages} />
       </InfiniteScroll>
     </div>
   );
