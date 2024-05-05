1cd8c73948af3f1c1fbf91be33905a8ea2eec3c2
Kamal <kamalabbas91@gmail.com>
2024-05-06 00:46:28 +0300

    Item detail page design updates

--- src/pages/ItemDetails.tsx
+++ src/pages/ItemDetails.tsx
@@ -12,6 +12,7 @@ export const ItemDetails = () => {
   const { data: videos } = useVideos(+id!);
 
   if (error) throw error;
+  // {getMostCommonColor(data?.backdrop_path!)}
 
   if(isLoading)
     return (
@@ -25,44 +26,40 @@ export const ItemDetails = () => {
       </div>
     );
 
+    
+
 
   if (!isLoading) {
     return (
       <div>
-        <div className="bg-cover bg-no-repeat bg-center relative" style={{backgroundImage: `url(${Imagehandler(data?.backdrop_path!, 'large')})`}}>
+        <div className="bg-cover bg-no-repeat relative" style={{backgroundImage: `url(${Imagehandler(data?.backdrop_path!, 'large')})`, backgroundPosition: `left calc((50vw - 170px) - 340px) top`}}>
           <div className="absolute inset-0 bg-gradient-to-r from-transparent to-black z-0"></div>
 
-          <div className="container flex gap-10 py-8 z-10 relative">
-            <div className="max-w-[18.75rem]">
-              <img className="rounded-xl"
-                src={Imagehandler(data?.poster_path!)}
-                alt=""
-              />
+          <div className="container flex gap-10 py-8 z-10 relative text-white">
+            <div className="shrink-0 max-w-[18.75rem]">
+              <img className="rounded-xl" src={Imagehandler(data?.poster_path!)} alt="alt"/>
             </div>
             <div className="details">
-              <div className="flex gap-2">
-                <h1>{data?.title}</h1><span>({data?.release_date.substring(0, 4)})</span>
-              </div>
+                <div className="flex gap-2 items-center">
+                  <h1 className="font-bold">{data?.title}</h1><span className="text-3xl opacity-60">({data?.release_date.substring(0, 4)})</span>
+                </div>
+                <div className="mt-2">
+                  {data?.release_date} - <span className="">{data?.genres.map((genre, i) => <span>{genre.name}{(i != data?.genres.length - 1 ) && ','} </span>)}</span> - {data?.runtime ? `${Math.floor(data?.runtime/60)}h ${Math.ceil((60 * Math.floor(data?.runtime/60)) - data?.runtime)}m` : ''}
+                </div>
 
-              <h2>{data?.tagline}</h2>
 
-              <div>
-                <h2>Overview</h2>
-                <p>{data?.overview}</p>
+              <h2 className="mt-6 italic opacity-60">{data?.tagline}</h2>
+
+              <div className="mt-2">
+                <h2 className="font-bold">Overview</h2>
+                <p className="mt-2">{data?.overview}</p>
               </div>
             </div>
           </div>
         </div>
 
         <div className="container mt-12">
-          <iframe
-            width="560"
-            height="315"
-            style={{maxWidth:'100%'}}
-            src={`https://www.youtube.com/embed/${
-              videos?.results.find((item) => item.type == "Trailer")?.key
-            }`}
-            title="YouTube video player"
+          <iframe className="w-full aspect-video" style={{maxWidth:'100%'}} src={`https://www.youtube.com/embed/${videos?.results.find((item) => item.type == "Trailer")?.key}`} title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           ></iframe>
         </div>
